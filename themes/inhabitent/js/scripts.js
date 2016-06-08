50d1ad4339b3a84329391879863b64c29690c513
Hunter Winterhalt <hunterwinterhalt@Hunters-MacBook-Air.local>
2016-06-08 09:36:14 -0700

    set positioning for label on journal image

---
+++ themes/inhabitent/js/scripts.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function( $ ) {
+    
+    $(".search-btn").on('click', function(){
+        event.preventDefault();
+        $(".search-field").toggle('slide');
+    });
+    
+});
